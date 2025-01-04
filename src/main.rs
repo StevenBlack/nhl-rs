81197dc3897209298442924bd391957e6003da4a
Steven Black <steveb@stevenblack.com>
2025-01-03 20:51:51 -0500

    Improve help text.

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,19 @@ use clap::Parser;
 #[derive(Parser,Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    // use local data
+    /// use local data
     #[arg(short, long)]
     local: bool,
 
-    // display the playoffs picture
+    /// display the playoffs picture
     #[arg(short, long)]
     playoffs: bool,
 
+    /// display the proximate schedule
     #[arg(short, long)]
     schedule: bool,
 
-    // Save sample data to file
+    /// Save sample data to file
     #[arg(long)]
     save: bool,
 }
